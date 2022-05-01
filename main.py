4fc0c0e00b5fc8a913703bb150ef7f43125e2265
kgleba <61940563+kgleba@users.noreply.github.com>
2022-05-01 14:22:18 +0300

    Code refactor

--- main.py
+++ main.py
@@ -327,23 +327,6 @@ def button_handler(call):
         elif call.data == 'clear: no':
             bot.edit_message_text('Запрос на очистку очереди публикаций отклонен.', MOD_ID,
                                   call.message.id, reply_markup=None)
-        elif call.data[:6] == 'reject':
-            actual_quote_id = int(call.data[8:])
-            bot.edit_message_text(f'{call.message.text}\n\nОтклонено модератором @{call.from_user.username}', MOD_ID,
-                                  call.message.id, reply_markup=None)
-            try:
-                pending.pop(actual_quote_id)
-            except KeyError:
-                pass
-        elif call.data == 'clear: yes':
-            save_json(dict(), 'queue.json')
-    
-            bot.edit_message_text('Успешно очистил очередь публикаций!', MOD_ID,
-                                  call.message.id, reply_markup=None)
-            push_gitlab('queue.json')
-        elif call.data == 'clear: no':
-            bot.edit_message_text('Запрос на очистку очереди публикаций отклонен.', MOD_ID,
-                                  call.message.id, reply_markup=None)
     
     bot.answer_callback_query(call.id)
     
