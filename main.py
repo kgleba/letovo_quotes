fe62a4bba31816accdab58a07e1d3711fd7b0faa
kgleba <61940563+kgleba@users.noreply.github.com>
2022-12-17 12:41:08 +0300

    Moderators list edit

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ MOD_ID = -1001791070494
 VOTING_ID = -1001645253084
 ADMIN_LIST = {1920379812: '@kglebaa', 1095891795: '@dr_platon', 1273466303: '@boris_ber', 1308606295: '@KSPalpatine'}
 MOD_LIST = {1224945213: '@DomineSalvaNos', 566239378: '@IvanFenster', 1050307229: '@GonSerg', 1711739283: '@Dr_Vortep',
-            1546943628: '@andrushkazbr'}
+            1546943628: '@VoN_is_a_true_hope'}
 MOD_LIST.update(ADMIN_LIST)
 
 BAN_TIME = 3600
