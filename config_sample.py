77686448faf8f7b3afc3453813f52dde411e6a90
kgleba <kgleba@yandex.ru>
2024-05-01 16:22:51 +0300

    Added config sample

---
+++ config_sample.py
@@ -0,0 +1,34 @@
+import os
+from pathlib import Path
+
+WEBHOOK_URL: str = 'https://channel.site/updates'
+SERVER: bool = 'SERVER' in os.environ
+SERVER_IP: str = '0.0.0.0'
+SERVER_PORT: int = os.getenv('PORT', 80)
+
+CHANNEL_NAME: str = 'Channel Name'
+CHANNEL_ID: str = '@channel_tag'
+ADMIN_ID: int = -1111111111111  # ID of the group with admins only
+VOTING_ID: int = -1111111111111  # ID of the group with moderators
+DISCUSSION_ID: int = -1111111111111  # ID of the channel's discussion
+
+BOT_TOKEN: str = os.getenv('BOT_TOKEN')  # token received from the BotFather
+
+BAN_TIME: int = 1  # default ban time, in hours
+ACCEPT: int = 3  # rating required for the quote to be accepted
+MIN_VOTES: int = 9  # minimum number of votes required to consider a quote in the verdict process
+
+ADMIN_LIST: dict[int, str] = {1111111111: '@example'}
+MOD_LIST: dict[int, str] = {}
+MOD_LIST.update(ADMIN_LIST)
+
+# Allows you to flexibly change the publishing time depending on the number of quotes in the queue
+# NB. The time must correspond to the time zone of the server
+POST_TIME: dict[str, int] = {'09:00': 0, '15:30': 9, '12:40': 16}
+# POST_TIME = {}
+VERDICT_TIME: str = '18:00'
+
+MAX_QUOTE_LEN: int = 500
+REJECTED_AUTHORS: list[str] = []  # should not appear in the hashtag
+
+DATA_FILES: list[str] = list(map(lambda e: e.name, Path('.').glob('*.json')))
